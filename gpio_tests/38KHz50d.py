0df25a0171ae2790b3ab13f7d62b8ae0a44fc1e5
Christoph Metz <loom@mopper.de>
2018-02-28 22:09:49 +0100

    basic tests for jay7583

---
+++ gpio_tests/38KHz50d.py
@@ -0,0 +1,24 @@
+import matrixio_hal
+
+BANK = 0
+SERVO_PIN = 0
+PRESCALER = 0x5
+
+bus = matrixio_hal.WishboneBus()
+bus.SpiInit()
+
+gpio = matrixio_hal.GPIOControl()
+gpio.Setup(bus)
+
+gpio.SetMode(SERVO_PIN, 1)
+gpio.SetFunction(SERVO_PIN, 1)
+gpio.SetPrescaler(BANK, PRESCALER)
+
+PWM_PERIOD = 1.0 / 38000
+FPGA_CLOCK = 125000000
+
+period_counter = int(PWM_PERIOD * FPGA_CLOCK // ((1 << PRESCALER) *2))
+
+bank = gpio.Bank(BANK)
+bank.SetPeriod(period_counter)
+bank.SetDuty(SERVO_PIN, int(period_counter/2))
